7a66ae21ff9d284d248d14207b3a1ca833be4169
Kim, Yerim <yerim343@naver.com>
2023-10-31 11:33:34 +0900

    initial commit - 초기 셋팅

---
+++ src/main/java/com/julook/JulookApplication.java
@@ -0,0 +1,13 @@
+package com.julook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JulookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JulookApplication.class, args);
+	}
+
+}
