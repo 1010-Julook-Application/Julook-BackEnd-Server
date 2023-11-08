482a38fe6721e37f4cc89b6dd5dfca3048f4d31d
Kim, Yerim <yerim343@naver.com>
2023-11-08 22:08:37 +0900

    wish update 오류 수정

--- src/main/java/com/julook/domain/user/repository/WishListRepositoryCustom.java
+++ src/main/java/com/julook/domain/user/repository/WishListRepositoryCustom.java
@@ -1,6 +1,7 @@
 package com.julook.domain.user.repository;
 
 import com.julook.domain.user.dto.request.WishRequestDTO;
+import com.julook.domain.user.entity.WishList;
 
 public interface WishListRepositoryCustom {
     // 찜 추가
@@ -8,4 +9,8 @@ public interface WishListRepositoryCustom {
 
     // 찜 삭제
     Boolean deleteWishList(WishRequestDTO userRequest);
+
+    // 찜 있는지 확인
+    WishList isAlreadyInWishList(Long userId, Long makNumber);
+
 }
